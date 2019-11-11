01d6a6bb0a9a5679972e122d1cda3c9428de5dc3
Andrei Cristian Petcu <andrei@ceata.org>
2019-11-11 23:58:04 +0200

    #2 get the counter in the URL bar for current webcompat/web-bugs

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,18 +5,18 @@ const prefixes = ["www.", "m.", "mobile.", "apps.", "free."];
 async function getMinimalDomain(domain) {
   let minimalDomain = domain;
   prefixes.forEach((prefix) => {
-    if(minimalDomain.indexOf(prefix) !== -1){
+    if (minimalDomain.indexOf(prefix) !== -1) {
       minimalDomain = minimalDomain.split(prefix)[1];
     }
   });
   return minimalDomain;
 }
 
-async function getIssuesApiPage(domain){
+async function getIssuesApiPage(domain) {
   let minimalDomain = await getMinimalDomain(domain);
   let query = `is%3Aissue%20is%3Aopen%20in%3Atitle%20${minimalDomain}`;
   prefixes.forEach((prefix) => {
-    query=`${query}%20OR%20in%3Atitle%20${prefix}${minimalDomain}`
+    query = `${query}%20OR%20in%3Atitle%20${prefix}${minimalDomain}`
   });
   return `https://api.github.com/search/issues?page=1&per_page=50&stage=all&sort=created&q=${query}%20state%3Aopen%20repo%3Awebcompat%2Fweb-bugs&order=desc`;
 }
@@ -25,13 +25,13 @@ async function getIssuesPage(domain) {
   let minimalDomain = await getMinimalDomain(domain);
   let query = `is%3Aissue+is%3Aopen+in%3Atitle+${minimalDomain}`;
   prefixes.forEach((prefix) => {
-    query=`${query}+OR+in%3Atitle+${prefix}${minimalDomain}`
+    query = `${query}+OR+in%3Atitle+${prefix}${minimalDomain}`
   });
   return `https://github.com/webcompat/web-bugs/issues?q=${query}`;
 }
 
-async function getIssuesForSite(site){
-  if(!siteCache[site]) {
+async function getIssuesForSite(site) {
+  if (!siteCache[site]) {
     const issuesPage = await getIssuesApiPage(site);
     const response = await fetch(issuesPage);
     const issuesAsJson = await response.json();
@@ -46,12 +46,12 @@ async function getIssuesForSite(site){
   return siteCache[site];
 }
 
-async function refreshDataForDomain(domain, tabId){
+async function refreshDataForDomain(domain, tabId) {
   const data = await getIssuesForSite(domain);
   let icon = "images/icon-16.png";
-  if(data.issuesCount > 0) {
+  if (data.issuesCount > 0) {
     let issueCount = "infinity";
-    if(data.issuesCount <= 9){
+    if (data.issuesCount <= 9) {
       issueCount = data.issuesCount;
     }
     icon = `images/count/${issueCount}.png`;
@@ -72,11 +72,25 @@ async function refreshDataForDomain(domain, tabId){
   chrome.pageAction.show(tabId);
 }
 
-async function onComplete(params){
+async function getDomainForUrl(url) {
+  let domain = new URL(url).host
+  try {
+    if (url.indexOf("https://github.com/webcompat/web-bugs/issues/") > -1) {
+      const issueId = url.split("https://github.com/webcompat/web-bugs/issues/")[1];
+      const issueResponse = await fetch(`https://api.github.com/repos/webcompat/web-bugs/issues/${issueId}`);
+      const issue = await issueResponse.json();
+      domain = issue.title.split(" - ")[0];
+    }
+  } finally {
+    return domain;
+  }
+}
+
+async function onComplete(params) {
   if (params.frameId !== 0) {
     return;
   }
-  const currentHost = new URL(params.url).host
+  let currentHost = await getDomainForUrl(params.url);
   refreshDataForDomain(currentHost, params.tabId);
 }
 
